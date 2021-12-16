fad6c0cd35a33650f36831dff165a608d2f44903
Chinmay Raut <craut@cn059.bio.sph.umich.edu>
2021-12-16 16:23:32 -0500

    started work on the gdrive port

---
+++ R/gdrive.R
@@ -0,0 +1,20 @@
+# gdrive.R
+# list of functions to connect to gdrive
+
+
+#' download file from gdrive
+#' 
+#' @param f_name string for what the file should be downloaded as
+#' @param share_url the share url of the file to download
+#' @param download_url the actual download url of the file to download
+#' @return string of the full file path of downloaded file
+#' 
+#' @examples 
+#' TODO: add nice examples
+download <- (f_name = "gdrive.file",
+             share_url = NULL,
+             download_url = NULL,
+             ...){
+  # TODO: finish writing this out :P
+  return()
+}
